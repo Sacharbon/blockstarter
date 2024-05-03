5aa971e98a227f5e729dda348631146018841a11
Valentin Peron <valentin.peron@epitech.eu>
2024-05-04 01:18:50 +0200

    Feat(SignIn.vue): Sign In view and router configuration

--- front/src/router/index.ts
+++ front/src/router/index.ts
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HelloWorld.vue'
+import SignInView from '../views/SignIn.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,6 +10,11 @@ const router = createRouter({
       name: 'home',
       component: HomeView
     },
+    {
+      path: '/Sign_in',
+      name: 'Sign_in',
+      component: SignInView
+    }
   ]
 })
 
