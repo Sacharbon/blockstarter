cd21c2f605c64f19d1710c0fda04c5ccf29174cf
sacharbon <sacha.dujardin@epitech.eu>
2024-05-03 22:53:30 +0200

    feat(front/*): initial commit!

---
+++ front/src/views/HelloWorld.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="container">
+    <h1>Haks 2024</h1>
+  </div>
+</template>
+
+<style scoped>
+#container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+h1 {
+  font-size: 3rem;
+}
+</style>
